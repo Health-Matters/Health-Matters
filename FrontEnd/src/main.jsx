f1b73c424e8e6ac842d1c3d222310041772b054b
MircoFernando <mircofdo@gmail.com>
2026-01-07 13:16:42 +0530

    initialized frontend project setup

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.jsx'
+import RootLayout from '@/layout/root-layout.jsx';
+import { LandingPage } from '@/pages/Landing-page/landing-page.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path='/' element={<LandingPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
